053258d89eeb2b12bdd5cd5af4117638a413fe71
Adnangad <gardobuyaadnan@gmail.com>
2024-07-02 05:01:31 -0700

    0-created a simple function that checks if boxes are unlockablew

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module contains a python function.
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Args:
+    boxes: the nested list.
+    """
+    ls = []
+    len_of_boxes = len(boxes)
+    for i in range(1, len_of_boxes):
+        ls.append(i)
+    count = -1
+    set_ls = set()
+    for i in boxes:
+        set_ls.update(i)
+    for x in ls:
+        if x not in set_ls:
+            return False
+    for i in boxes:
+        count = count + 1
+        for j in i:
+            if j == count and (sum(row.count(j) for row in boxes) <= 1):
+                return False
+    return True
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,1 @@
+This folder contains code that checks if boxes in a list are unlockable
