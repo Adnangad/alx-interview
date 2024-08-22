ea4875cc4f70e1765e766a09e76b3325f028136d
Adnangad <gardobuyaadnan@gmail.com>
2024-08-22 19:34:26 +0300

    0-make change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,15 +6,13 @@ def makeChange(coins, total):
     """Solves make change problem"""
     if total <= 0:
         return 0
-    coins.sort(reversed=True)
-    i = len(coins) - 1
+    coins.sort(reverse=True)
     count = 0
     result = total
-    while i > 0:
-        result = result - coins[i]
-        count += 1
-        if result < coins[i]:
-            i = i - 1
+    for coin in coins:
+        while result >= coin:
+            result -= coin
+            count += 1
     if result == 0:
         return count
     else:
